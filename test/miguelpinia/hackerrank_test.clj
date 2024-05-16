abc03065195fb202cb3f0a764c62a65b6e0eb93f
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-05-16 17:16:23 -0600

    Init commit.

---
+++ test/miguelpinia/hackerrank_test.clj
@@ -0,0 +1,7 @@
+(ns miguelpinia.hackerrank-test
+  (:require [clojure.test :refer :all]
+            [miguelpinia.hackerrank :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
