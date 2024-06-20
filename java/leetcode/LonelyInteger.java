b8e8fb14520a17ea756dfbe50b26e27209626508
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-06-20 14:00:00 -0600

    Lonely integer problem solved in Java.

---
+++ java/leetcode/LonelyInteger.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * https://www.hackerrank.com/challenges/lonely-integer/problem
+ *
+ * @author miguel
+ */
+public class LonelyInteger {
+
+    public static int lonelyinteger(List<Integer> a) {
+        // Write your code here
+        return a.stream().reduce((x, y) -> x ^ y).get();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        int result = lonelyinteger(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+}
