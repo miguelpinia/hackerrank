16a38ccea762a60ab84628d1f9bd1413e1142f35
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-09-13 12:04:00 -0600

    Solved leetcode problem 70. Climbing Stairs.

---
+++ java/leetcode/ClimbStairs.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2024 miguel.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    miguel - initial API and implementation and/or initial documentation
+ */
+
+/**
+ * 70. Climbing Stairs
+ *
+ * You are climbing a staircase. It takes n steps to reach the top.
+ *
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: n = 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ * Example 2:
+ *
+ * Input: n = 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ *
+ * @author miguel
+ */
+public class ClimbStairs {
+
+}
