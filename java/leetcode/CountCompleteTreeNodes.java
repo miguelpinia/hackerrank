4f714c17be1ba6dc941b035c733dbbce455c465a
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-09-14 12:03:00 -0600

    Solved leetcode problem Count Complete Tree Nodes.

---
+++ java/leetcode/CountCompleteTreeNodes.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2024 miguel.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    miguel - initial API and implementation and/or initial documentation
+ */
+
+/**
+ *
+ * @author miguel
+ */
+public class CountCompleteTreeNodes {
+
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
+}
