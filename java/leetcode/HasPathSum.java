a849569996009acebe6b42286d5667d84401a039
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-09-15 12:03:00 -0600

    Solved leetcode problem 112. Path Sum.

---
+++ java/leetcode/HasPathSum.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2024 miguel.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    miguel - initial API and implementation and/or initial documentation
+ */
+
+/**
+ * 112. Path Sum
+ *
+ * Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+ *
+ * A leaf is a node with no children.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+ * Output: true
+ * Explanation: The root-to-leaf path with the target sum is shown.
+ * Example 2:
+ *
+ *
+ * Input: root = [1,2,3], targetSum = 5
+ * Output: false
+ * Explanation: There two root-to-leaf paths in the tree:
+ * (1 --> 2): The sum is 3.
+ * (1 --> 3): The sum is 4.
+ * There is no root-to-leaf path with sum = 5.
+ * Example 3:
+ *
+ * Input: root = [], targetSum = 0
+ * Output: false
+ * Explanation: Since the tree is empty, there are no root-to-leaf paths.
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the tree is in the range [0, 5000].
+ * -1000 <= Node.val <= 1000
+ * -1000 <= targetSum <= 1000
+ *
+ * @author miguel
+ */
+public class HasPathSum {
+
+    /**
+     * 0ms, beats 100%. 42.98mb, beats 63.72%.
+     *
+     *
+     * @param root
+     * @param targetSum
+     * @return
+     */
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        int newTarget = targetSum - root.val;
+        if (root.left == null && root.right == null) {
+            return newTarget == 0;
+        }
+        return hasPathSum(root.left, newTarget) || hasPathSum(root.right, newTarget);
+    }
+
+}
