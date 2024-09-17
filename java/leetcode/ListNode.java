0a801ad6658f3b553bdc5cf0a7d97ab1986ba764
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-09-17 12:02:00 -0600

    ListNode class helper.

---
+++ java/leetcode/ListNode.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+/*
+ * Copyright (c) 2024 miguel.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    miguel - initial API and implementation and/or initial documentation
+ */
+/**
+ *
+ * @author miguel
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
