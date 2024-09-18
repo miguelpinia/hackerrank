45654d135c38c6eb3ec3e82def0fa1b2c6b1f59a
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-09-18 12:02:00 -0600

    Solved leetcode problem 236. Lowest Common Ancestor of Binary Tree.

---
+++ java/leetcode/LowestCommonAncestorSimplified.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2024 miguel.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    miguel - initial API and implementation and/or initial documentation
+ */
+
+/**
+ * 236. Lowest Common Ancestor of a Binary Tree
+ *
+ * Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+ *
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * Output: 3
+ * Explanation: The LCA of nodes 5 and 1 is 3.
+ * Example 2:
+ *
+ *
+ * Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ * Output: 5
+ * Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+ * Example 3:
+ *
+ * Input: root = [1,2], p = 1, q = 2
+ * Output: 1
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the tree is in the range [2, 105].
+ * -109 <= Node.val <= 109
+ * All Node.val are unique.
+ * p != q
+ * p and q will exist in the tree.
+ *
+ * @author miguel
+ */
+public class LowestCommonAncestorSimplified {
+
+    private TreeNode search(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == root || q == root) {
+            return root;
+        }
+        TreeNode left = search(root.left, p, q);
+        TreeNode right = search(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left == null) {
+            return right;
+        }
+        return left;
+    }
+
+    /**
+     * 6ms, beats 100%, 44.75mb, beats 44.86%.
+     *
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return search(root, p, q);
+    }
+
+}
