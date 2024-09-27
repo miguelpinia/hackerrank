6b7ac604bca237ad54e59386fbf1eca6d2a516c4
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-09-27 12:02:00 -0600

    TreeNode class helper.

---
+++ java/leetcode/TreeNode.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+/*
+ * Copyright (c) 2024 miguel.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    miguel - initial API and implementation and/or initial documentation
+ */
+/**
+ *
+ * @author miguel
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
