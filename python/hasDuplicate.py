31cecf1b774e1e22e7a8fe22ed6ffb25147a60a2
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-09-29 12:01:00 -0600

    Neetcode (python): Has duplicates

---
+++ python/hasDuplicate.py
@@ -0,0 +1,13 @@
+# Duplicate Integer
+# Solved
+# Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+# Example 1:
+# Input: nums = [1, 2, 3, 3]
+# Output: true
+# Example 2:
+# Input: nums = [1, 2, 3, 4]
+# Output: false
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+         return len(set(nums)) != len(nums)
