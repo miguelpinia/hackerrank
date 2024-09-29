9f477131758e5acffe409a5f95776ae57bf98780
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-09-29 12:00:00 -0600

    Solved leetcode problem 6. ZigZag Conversion.

---
+++ java/leetcode/ZigZagConversion.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2024 miguel.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    miguel - initial API and implementation and/or initial documentation
+ */
+
+/**
+ * 6. Zigzag Conversion
+ *
+ *
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+ *
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ *
+ * Write the code that will take a string and make this conversion given a number of rows:
+ *
+ * string convert(string s, int numRows);
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "PAYPALISHIRING", numRows = 3
+ * Output: "PAHNAPLSIIGYIR"
+ * Example 2:
+ *
+ * Input: s = "PAYPALISHIRING", numRows = 4
+ * Output: "PINALSIGYAHRPI"
+ * Explanation:
+ * P     I    N
+ * A   L S  I G
+ * Y A   H R
+ * P     I
+ * Example 3:
+ *
+ * Input: s = "A", numRows = 1
+ * Output: "A"
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 1000
+ * s consists of English letters (lower-case and upper-case), ',' and '.'.
+ * 1 <= numRows <= 1000
+ *
+ * @author miguel
+ */
+public class ZigZagConversion {
+
+    /**
+     * 3ms, beats 87.32%, 44.64mb, beats 82.96%.
+     *
+     * @param s
+     * @param numRows
+     * @return
+     */
+    public String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int row = 0; row < numRows; row++) {
+            int increment = 2 * (numRows - 1);
+            for (int i = row; i < s.length(); i += increment) {
+                sb.append(s.charAt(i));
+                if (row > 0 && row < (numRows - 1)
+                        && (i + increment - 2 * row) < s.length()) {
+                    sb.append(s.charAt(i + increment - 2 * row));
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+}
