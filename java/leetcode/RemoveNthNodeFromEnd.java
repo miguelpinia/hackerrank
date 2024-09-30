e22454453a77e3b1ac65374d41152674f0d05d7a
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-09-30 08:18:35 -0600

    Solved leetcode problem 19. Remove nth. node from end of list.

---
+++ java/leetcode/RemoveNthNodeFromEnd.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2024 miguel.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    miguel - initial API and implementation and/or initial documentation
+ */
+
+/**
+ * 19. Remove Nth Node From End of List
+ *
+ *
+ * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [1,2,3,4,5], n = 2
+ * Output: [1,2,3,5]
+ * Example 2:
+ *
+ * Input: head = [1], n = 1
+ * Output: []
+ * Example 3:
+ *
+ * Input: head = [1,2], n = 1
+ * Output: [1]
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the list is sz.
+ * 1 <= sz <= 30
+ * 0 <= Node.val <= 100
+ * 1 <= n <= sz
+ *
+ *
+ * Follow up: Could you do this in one pass?
+ *
+ * @author miguel
+ */
+public class RemoveNthNodeFromEnd {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        if (fast == null) {
+            return head.next;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+        return head;
+    }
+
+    class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
