4ee5db4e5cedff1d91fa26ef840f1fe6cf2902b4
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-09-29 22:03:00 -0600

    Solved leetcode problem 155. Min stack using nodes.

---
+++ java/leetcode/MinStackNode.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2024 miguel.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    miguel - initial API and implementation and/or initial documentation
+ */
+
+/**
+ *
+ * @author miguel
+ */
+public class MinStackNode {
+
+    Node top;
+
+    public MinStackNode() {
+    }
+
+    public void push(int val) {
+        if (top == null) {
+            top = new Node(val, val, null);
+        } else {
+            top = new Node(val, Math.min(val, top.min), top);
+        }
+    }
+
+    public void pop() {
+        top = top.next;
+    }
+
+    public int top() {
+        return top.val;
+    }
+
+    public int getMin() {
+        return top.min;
+    }
+
+    class Node {
+
+        int val;
+        int min;
+        Node next;
+
+        Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+}
