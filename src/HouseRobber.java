e55cfbafd42bd1c8d243541e4f0e682ece96be7f
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-10-02 12:00:00 -0600

    Problem solved leetcode 198. House robber.

---
+++ src/HouseRobber.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2024 miguel.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    miguel - initial API and implementation and/or initial documentation
+ */
+
+/**
+ * 198. House Robber
+ * Solved
+ * Medium
+ *
+ * Topics Array Dynamic Programming
+ *
+ * Companies
+ * You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+ *
+ * Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,3,1]
+ * Output: 4
+ * Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+ * Total amount you can rob = 1 + 3 = 4.
+ * Example 2:
+ *
+ * Input: nums = [2,7,9,3,1]
+ * Output: 12
+ * Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+ * Total amount you can rob = 2 + 9 + 1 = 12.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 400
+ *
+ * @author miguel
+ */
+public class HouseRobber {
+
+    public int rob(int[] nums) {
+        int r1 = 0;
+        int r2 = 0;
+        int temp;
+        for (int n : nums) {
+            temp = Math.max(n + r1, r2);
+            r1 = r2;
+            r2 = temp;
+        }
+        return r2;
+    }
+
+}
