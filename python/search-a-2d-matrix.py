69bc3304fb596ad0c65bc98feed2aea1cb5ac626
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-10-11 12:00:00 -0600

    Problem solved leetcode 74. Search a 2D matrix.

---
+++ python/search-a-2d-matrix.py
@@ -0,0 +1,54 @@
+# 74. Search a 2D Matrix
+# Solved
+# Medium
+
+# Topics
+# Array
+# Binary Search
+# Matrix
+
+# Companies
+# You are given an m x n integer matrix matrix with the following two properties:
+
+# Each row is sorted in non-decreasing order.
+# The first integer of each row is greater than the last integer of the previous row.
+# Given an integer target, return true if target is in matrix or false otherwise.
+
+# You must write a solution in O(log(m * n)) time complexity.
+
+
+
+# Example 1:
+
+
+# Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+# Output: true
+# Example 2:
+
+
+# Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+# Output: false
+
+
+# Constraints:
+
+# m == matrix.length
+# n == matrix[i].length
+# 1 <= m, n <= 100
+# -104 <= matrix[i][j], target <= 104
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows = len(matrix)
+        cols = len(matrix[0])
+        size = rows * cols
+        left, right = 0, size - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            r = mid // cols
+            c = mid % cols
+            current = matrix[r][c]
+            if current == target: return True
+            elif current > target: right = mid - 1
+            else: left = mid + 1
+
+        return False
